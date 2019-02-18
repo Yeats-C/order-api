b4fe5ec4628ddfdce330554ea48cc6c385f9ea07
huangjiaojiao <huangjiaojiao@aiqin.com>
2019-02-18 15:00:59 +0800

    修改新增订单接口

--- src/main/java/com/aiqin/mgs/order/api/domain/response/OrderNoCodeResponse/SelectSaleViewResonse.java
+++ src/main/java/com/aiqin/mgs/order/api/domain/response/OrderNoCodeResponse/SelectSaleViewResonse.java
@@ -46,6 +46,16 @@ public class SelectSaleViewResonse{
 	@JsonProperty("passenger_flow")
 	private Integer passengerFlow;
 	
+	
+	
+	public String getTypeName() {
+		return typeName;
+	}
+
+	public void setTypeName(String typeName) {
+		this.typeName = typeName;
+	}
+
 	public String getTypeId() {
 		return typeId;
 	}
