9f814911d7542472b00df35efa255e661a53cb29
tao <tao.zhang@biz-united.com.cn>
2019-03-14 17:31:09 +0800

    fix

--- src/main/java/com/aiqin/mgs/order/api/domain/request/orderList/OrderProductReqVo.java
+++ src/main/java/com/aiqin/mgs/order/api/domain/request/orderList/OrderProductReqVo.java
@@ -107,6 +107,10 @@ public class OrderProductReqVo implements Serializable {
     @JsonProperty("weight")
     private Integer weight = 0;
 
+    @JsonProperty("product_type")
+    @ApiModelProperty("商品类型，1-正常品，0-效期品")
+    private Integer productType=1;
+
     @ApiModelProperty(value = "优惠额度信息")
     @JsonProperty("discount_amount_info")
     private List<DiscountAmountInfo> discountAmountInfo;
