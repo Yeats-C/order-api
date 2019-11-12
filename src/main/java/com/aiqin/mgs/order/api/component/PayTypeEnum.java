97a779569c37b25e1ad6502fa3ae2948cd503cc0
Tao.Chen <talbotgrant@foxmail.com>
2019-11-12 15:12:30 +0800

    枚举类

--- src/main/java/com/aiqin/mgs/order/api/component/PayTypeEnum.java
+++ src/main/java/com/aiqin/mgs/order/api/component/PayTypeEnum.java
@@ -1,23 +1,74 @@
 package com.aiqin.mgs.order.api.component;
 
 import io.swagger.annotations.ApiModel;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@AllArgsConstructor
 @Getter
 @ApiModel("支付类型")
 public enum PayTypeEnum {
-    ONLINE_WE_CHAT(0,"在线支付-微信"),
-    ONLINE_ALI_PAY(1,"在线支付-支付宝"),
-    ONLINE_BANK_CARD(2,"在线支付-银行卡"),
-    CASH(3,"到店支付-现金"),
-    DOOR_WE_CHAT(4,"到店支付-微信"),
-    DOOR_ALI_PAY(5,"到店支付-支付宝"),
-    DOOR_BANK_CARD(6,"到店支付-银行卡"),
-    BALANCE_PAY(7,"储值卡支付");
-    private Integer code;
 
+    /***在线支付-微信*/
+    ONLINE_WE_CHAT(0, "0", "在线支付-微信"),
+    /***在线支付-支付宝*/
+    ONLINE_ALI_PAY(1, "1", "在线支付-支付宝"),
+    /***在线支付-银行卡*/
+    ONLINE_BANK_CARD(2, "2", "在线支付-银行卡"),
+    /***到店支付-现金*/
+    CASH(3, "3", "到店支付-现金"),
+    /***到店支付-微信*/
+    DOOR_WE_CHAT(4, "4", "到店支付-微信"),
+    /***到店支付-支付宝*/
+    DOOR_ALI_PAY(5, "5", "到店支付-支付宝"),
+    /***到店支付-银行卡*/
+    DOOR_BANK_CARD(6, "6", "到店支付-银行卡"),
+    /***储值卡支付*/
+    BALANCE_PAY(7, "7", "储值卡支付");
+
+    private Integer code;
+    private String value;
     private String desc;
+
+    PayTypeEnum(Integer code, String value, String desc) {
+        this.code = code;
+        this.value = value;
+        this.desc = desc;
+    }
+
+    public static PayTypeEnum getEnum(Integer code) {
+        for (PayTypeEnum type : PayTypeEnum.values()) {
+            if (type.getCode().equals(code)) {
+                return type;
+            }
+        }
+        return null;
+    }
+
+    public static PayTypeEnum getEnum(String value) {
+        for (PayTypeEnum type : PayTypeEnum.values()) {
+            if (type.getValue().equals(value)) {
+                return type;
+            }
+        }
+        return null;
+    }
+
+
+    public static String getEnumDesc(Integer code) {
+        for (PayTypeEnum type : PayTypeEnum.values()) {
+            if (type.getCode().equals(code)) {
+                return type.getDesc();
+            }
+        }
+        return "";
+    }
+
+    public static String getEnumDesc(String value) {
+        for (PayTypeEnum type : PayTypeEnum.values()) {
+            if (type.getValue().equals(value)) {
+                return type.getDesc();
+            }
+        }
+        return "";
+    }
 
 }
