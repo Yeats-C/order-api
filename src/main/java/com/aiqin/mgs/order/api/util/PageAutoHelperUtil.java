6c582c6f6d83686ec5dea01dfbde7b4a67825638
Tao.Chen <talbotgrant@foxmail.com>
2019-11-12 15:08:38 +0800

    分页工具

--- src/main/java/com/aiqin/mgs/order/api/util/PageAutoHelperUtil.java
+++ src/main/java/com/aiqin/mgs/order/api/util/PageAutoHelperUtil.java
@@ -1,5 +1,6 @@
 package com.aiqin.mgs.order.api.util;
 
+import com.aiqin.mgs.order.api.base.PageResData;
 import com.aiqin.mgs.order.api.base.PagesRequest;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
@@ -36,5 +37,28 @@ public class PageAutoHelperUtil {
         List list = supplier.get();
         return new PageInfo(list);
     }
+
+    /**
+     * 查询列表分页方法
+     *
+     * @param supplier 查询dao层方法一起写入
+     * @param page     分页量
+     * @return com.github.pagehelper.PageInfo
+     * @author: Tao.Chen
+     * @version: v1.0.0
+     * @date 2019/11/6 16:24
+     */
+    public static PageResData generatePageRes(Supplier<List> supplier, PagesRequest page) {
+        if (page == null) {
+            page = new PagesRequest();
+        }
+        int pageNum = page.getBeginIndex();
+        int pageSize = page.getPageSize();
+        PageHelper.startPage(pageNum, pageSize);
+        List list = supplier.get();
+        PageInfo pageInfo = new PageInfo(list);
+        return new PageResData((int) pageInfo.getTotal(), pageInfo.getList());
+    }
+
 
 }
