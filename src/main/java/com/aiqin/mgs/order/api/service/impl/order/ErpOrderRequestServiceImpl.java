61ff4b26186b3f539ae3a22205de5a07f183f836
chentao <talbotgrant@foxmail.com>
2019-12-10 14:46:39 +0800

    订单代码整改

--- src/main/java/com/aiqin/mgs/order/api/service/impl/order/ErpOrderRequestServiceImpl.java
+++ src/main/java/com/aiqin/mgs/order/api/service/impl/order/ErpOrderRequestServiceImpl.java
@@ -86,17 +86,6 @@ public class ErpOrderRequestServiceImpl implements ErpOrderRequestService {
         return storeInfo;
     }
 
-    /**
-     * 查询商品明细
-     *
-     * @param storeId   门店id
-     * @param productId 商品id
-     * @param skuCode   商品sku编码
-     * @return com.aiqin.mgs.order.api.domain.ProductInfo
-     * @author: Tao.Chen
-     * @version: v1.0.0
-     * @date 2019/12/2 15:37
-     */
     @Override
     public ProductInfo getProductDetail(String storeId, String productId, String skuCode) {
         ProductInfo product = new ProductInfo();
@@ -155,15 +144,6 @@ public class ErpOrderRequestServiceImpl implements ErpOrderRequestService {
 
     }
 
-    /**
-     * 根据订单编码获取订单支付状态
-     *
-     * @param payId 支付id
-     * @return com.aiqin.mgs.order.api.component.enums.ErpPayStatusEnum
-     * @author: Tao.Chen
-     * @version: v1.0.0
-     * @date 2019/12/2 15:37
-     */
     @Override
     public ErpOrderPayStatusResponse getOrderPayStatus(String payId) {
         ErpOrderPayStatusResponse response = new ErpOrderPayStatusResponse();
@@ -183,16 +163,6 @@ public class ErpOrderRequestServiceImpl implements ErpOrderRequestService {
         return response;
     }
 
-    /**
-     * 发起支付请求
-     *
-     * @param order    订单信息
-     * @param orderPay 订单支付信息
-     * @return boolean
-     * @author: Tao.Chen
-     * @version: v1.0.0
-     * @date 2019/12/2 15:37
-     */
     @Override
     public boolean sendPayRequest(ErpOrderInfo order, ErpOrderPay orderPay) {
         boolean flag = false;
@@ -209,15 +179,6 @@ public class ErpOrderRequestServiceImpl implements ErpOrderRequestService {
         return flag;
     }
 
-    /**
-     * 获取物流券
-     *
-     * @param order 订单信息
-     * @return java.math.BigDecimal
-     * @author: Tao.Chen
-     * @version: v1.0.0
-     * @date 2019/12/2 15:45
-     */
     @Override
     public BigDecimal getGoodsCoupon(ErpOrderInfo order) {
         BigDecimal goodsCoupon = null;
@@ -234,15 +195,6 @@ public class ErpOrderRequestServiceImpl implements ErpOrderRequestService {
         return goodsCoupon;
     }
 
-    /**
-     * 订单支付成功推送供应链
-     *
-     * @param order
-     * @return boolean
-     * @author: Tao.Chen
-     * @version: v1.0.0
-     * @date 2019/12/2 15:48
-     */
     @Override
     public ErpOrderInfo sendOrderToSupplyChainAndGetSplitGroup(ErpOrderInfo order) {
         ErpOrderInfo resultInfo = new ErpOrderInfo();
@@ -258,16 +210,6 @@ public class ErpOrderRequestServiceImpl implements ErpOrderRequestService {
         return resultInfo;
     }
 
-    /**
-     * 推送拆分后的订单到供应链
-     *
-     * @param order
-     * @param splitOrderList
-     * @return boolean
-     * @author: Tao.Chen
-     * @version: v1.0.0
-     * @date 2019/12/3 17:26
-     */
     @Override
     public boolean sendSplitOrderToSupplyChain(ErpOrderInfo order, List<ErpOrderInfo> splitOrderList) {
         boolean flag = false;
@@ -284,16 +226,6 @@ public class ErpOrderRequestServiceImpl implements ErpOrderRequestService {
         return flag;
     }
 
-    /**
-     * 商品销售区域配置校验
-     *
-     * @param storeInfo
-     * @param orderProductItemList
-     * @return boolean
-     * @author: Tao.Chen
-     * @version: v1.0.0
-     * @date 2019/12/2 16:11
-     */
     @Override
     public boolean areaCheck(StoreInfo storeInfo, List<ErpOrderItem> orderProductItemList) {
         boolean flag = false;
@@ -310,16 +242,6 @@ public class ErpOrderRequestServiceImpl implements ErpOrderRequestService {
         return flag;
     }
 
-    /**
-     * 商品库存校验
-     *
-     * @param storeInfo
-     * @param orderProductItemList
-     * @return boolean
-     * @author: Tao.Chen
-     * @version: v1.0.0
-     * @date 2019/12/2 16:11
-     */
     @Override
     public boolean repertoryCheck(StoreInfo storeInfo, List<ErpOrderItem> orderProductItemList) {
         boolean flag = false;
@@ -336,16 +258,6 @@ public class ErpOrderRequestServiceImpl implements ErpOrderRequestService {
         return flag;
     }
 
-    /**
-     * 商品销售价格校验
-     *
-     * @param storeInfo
-     * @param orderProductItemList
-     * @return boolean
-     * @author: Tao.Chen
-     * @version: v1.0.0
-     * @date 2019/12/2 16:11
-     */
     @Override
     public boolean priceCheck(StoreInfo storeInfo, List<ErpOrderItem> orderProductItemList) {
         boolean flag = false;
@@ -362,16 +274,6 @@ public class ErpOrderRequestServiceImpl implements ErpOrderRequestService {
         return flag;
     }
 
-    /**
-     * 促销活动校验
-     *
-     * @param storeInfo
-     * @param orderProductItemList
-     * @return boolean
-     * @author: Tao.Chen
-     * @version: v1.0.0
-     * @date 2019/12/2 16:11
-     */
     @Override
     public boolean activityCheck(StoreInfo storeInfo, List<ErpOrderItem> orderProductItemList) {
         boolean flag = false;
