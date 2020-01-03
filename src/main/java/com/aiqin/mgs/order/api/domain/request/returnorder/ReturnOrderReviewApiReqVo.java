4332e66bdc4dc028d93d46f932f5a4fbaada8d5a
hantao <hantaoemail@sina.cn>
2020-01-03 19:10:48 +0800

    提供给供应链--同步是否成功（创建退供单）修改

--- src/main/java/com/aiqin/mgs/order/api/domain/request/returnorder/ReturnOrderReviewApiReqVo.java
+++ src/main/java/com/aiqin/mgs/order/api/domain/request/returnorder/ReturnOrderReviewApiReqVo.java
@@ -1,14 +1,11 @@
 package com.aiqin.mgs.order.api.domain.request.returnorder;
 
-import com.aiqin.mgs.order.api.domain.ReturnOrderDetail;
-import com.alibaba.fastjson.annotation.JSONField;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
 import java.io.Serializable;
-import java.util.List;
 
 /**
  * 供应链调用实体类
@@ -34,12 +31,12 @@ public class ReturnOrderReviewApiReqVo implements Serializable {
     @ApiModelProperty("退货单状态(供应链使用):4-等待退货验收，5-等待退货入库 11-退货完成")
     private Integer operateStatus;
 
-    @NotBlank(message = "加盟商id")
-    @ApiModelProperty("加盟商id")
-    private String franchiseeId;
+//    @NotBlank(message = "加盟商id")
+//    @ApiModelProperty("加盟商id")
+//    private String franchiseeId;
 
-    @ApiModelProperty(value="退货单详情")
-    private List<ReturnOrderDetail> details;
+//    @ApiModelProperty(value="退货单详情")
+//    private List<ReturnOrderDetail> details;
 
     @ApiModelProperty(value="修改退货单详情实退数量")
     private ReturnOrderDetailReviewApiReqVo returnOrderDetailReviewApiReqVo;
