ca04fdb42f243df6f47181b04de2aa8ac51c7fc4
jinghaibo <jinghaibo@fotrontimes.com>
2020-01-19 16:17:31 +0800

    预存订单未付款不能退货

--- src/main/java/com/aiqin/mgs/order/api/service/impl/OrderDetailServiceImpl.java
+++ src/main/java/com/aiqin/mgs/order/api/service/impl/OrderDetailServiceImpl.java
@@ -441,7 +441,12 @@ public class OrderDetailServiceImpl implements OrderDetailService {
             }
 
             //为pos端判断是否可退货
-            info.setTurnReturnView(checkTurn(detailList));
+            if (orderInfo.getOrderStatus()==2){
+                info.setTurnReturnView(checkTurn(detailList));
+            }else {
+                info.setTurnReturnView(1);
+            }
+
             info.setDetailList(detailList);
 
         } catch (Exception e) {
