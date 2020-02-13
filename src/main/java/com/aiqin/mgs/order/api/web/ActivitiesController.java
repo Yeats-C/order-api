445744ff469086de15ec493eb1bafeda9f87c976
chensifang <762316017@qq.com>
2020-02-13 17:29:32 +0800

    促销活动列表接口完成

--- src/main/java/com/aiqin/mgs/order/api/web/ActivitiesController.java
+++ src/main/java/com/aiqin/mgs/order/api/web/ActivitiesController.java
@@ -3,7 +3,7 @@ package com.aiqin.mgs.order.api.web;
 
 import com.aiqin.ground.util.protocol.http.HttpResponse;
 import com.aiqin.mgs.order.api.domain.Activities;
-import com.aiqin.mgs.order.api.domain.CartOrderInfo;
+import com.aiqin.mgs.order.api.service.ActivitesService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 @RestController
@@ -20,16 +21,18 @@ import java.util.List;
 public class ActivitiesController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ActivitiesController.class);
+    @Resource
+    private ActivitesService activitesService;
 
     /**
      * 促销活动管理--活动列表
      * @param activities
      * @return
      */
-    @PostMapping("/ActivityList")
-    @ApiOperation(value = "显示勾选商品列表")
-    public HttpResponse<List<CartOrderInfo>> displayCartLineCheckProduct(Activities activities){
-        return null;
+    @PostMapping("/activityList")
+    @ApiOperation(value = "促销活动管理--活动列表")
+    public HttpResponse<List<Activities>> activityList(Activities activities){
+        return activitesService.activityList(activities);
     }
 
 }
