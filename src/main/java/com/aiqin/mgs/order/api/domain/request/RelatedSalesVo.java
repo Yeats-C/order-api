51499ce8769a800697c721905e481cb9218387b7
hantao <hantaoemail@sina.cn>
2020-02-14 14:29:34 +0800

    修改实体类

--- src/main/java/com/aiqin/mgs/order/api/domain/request/RelatedSalesVo.java
+++ src/main/java/com/aiqin/mgs/order/api/domain/request/RelatedSalesVo.java
@@ -1,6 +1,8 @@
 package com.aiqin.mgs.order.api.domain.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 /**
@@ -13,8 +15,12 @@ import lombok.Data;
 @ApiModel("关联销售查询")
 public class RelatedSalesVo {
 
+    @ApiModelProperty(value = "销售品类ID")
+    @JsonProperty("salse_category_id")
     private String salseCategoryId;
 
+    @ApiModelProperty(value = "生效状态 0:生效 1：失效")
+    @JsonProperty("status")
     private Integer status;
 
 }
