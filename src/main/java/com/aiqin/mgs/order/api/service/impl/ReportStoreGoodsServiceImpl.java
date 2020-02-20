43570c0e70ee62a93b1cf38771ffdf4eba7ffa17
hantao <hantaoemail@sina.cn>
2020-02-20 10:33:51 +0800

    门店补货列表商品详情查询接口

--- src/main/java/com/aiqin/mgs/order/api/service/impl/ReportStoreGoodsServiceImpl.java
+++ src/main/java/com/aiqin/mgs/order/api/service/impl/ReportStoreGoodsServiceImpl.java
@@ -3,12 +3,17 @@ package com.aiqin.mgs.order.api.service.impl;
 import com.aiqin.mgs.order.api.base.PageRequestVO;
 import com.aiqin.mgs.order.api.base.PageResData;
 import com.aiqin.mgs.order.api.dao.ReportStoreGoodsDao;
+import com.aiqin.mgs.order.api.dao.ReportStoreGoodsDetailDao;
 import com.aiqin.mgs.order.api.domain.ReportStoreGoods;
+import com.aiqin.mgs.order.api.domain.ReportStoreGoodsDetail;
+import com.aiqin.mgs.order.api.domain.request.ReportStoreGoodsDetailVo;
 import com.aiqin.mgs.order.api.domain.request.ReportStoreGoodsVo;
+import com.aiqin.mgs.order.api.domain.response.report.ReportStoreGoodsCountDetailResponse;
 import com.aiqin.mgs.order.api.service.ReportStoreGoodsService;
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -27,6 +32,8 @@ public class ReportStoreGoodsServiceImpl implements ReportStoreGoodsService {
 
     @Autowired
     private ReportStoreGoodsDao reportStoreGoodsDao;
+    @Autowired
+    private ReportStoreGoodsDetailDao reportStoreGoodsDetailDao;
 
     @Override
     public Boolean insert(ReportStoreGoods entity) {
@@ -47,5 +54,13 @@ public class ReportStoreGoodsServiceImpl implements ReportStoreGoodsService {
         result.setTotalCount((int)((Page)reportStoreGoods).getTotal());
         result.setDataList(reportStoreGoods);
         return result;
+    }
+
+    @Override
+    public List<ReportStoreGoodsDetail> getCountDetailList(ReportStoreGoodsDetailVo searchVo) {
+        if(StringUtils.isNotBlank(searchVo.getStoreCode())&&StringUtils.isNotBlank(searchVo.getBrandId())&&StringUtils.isNotBlank(searchVo.getCountTime())){
+            return reportStoreGoodsDetailDao.selectList(searchVo);
+        }
+        return null;
     }
 }
