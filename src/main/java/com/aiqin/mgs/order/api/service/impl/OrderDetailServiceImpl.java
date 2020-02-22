d606ea35d896f9eb763ee3bb20629e590ee57a48
jinghaibo <jinghaibo@fotrontimes.com>
2020-02-22 20:05:12 +0800

    批量提交订单 返回

--- src/main/java/com/aiqin/mgs/order/api/service/impl/OrderDetailServiceImpl.java
+++ src/main/java/com/aiqin/mgs/order/api/service/impl/OrderDetailServiceImpl.java
@@ -920,7 +920,7 @@ public class OrderDetailServiceImpl implements OrderDetailService {
 
         }
 
-        return HttpResponse.success();
+        return HttpResponse.successGenerics(cartInfo);
     }
 
     @Override
