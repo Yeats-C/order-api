42c5e1177a1cb1c5ca8c22ac5cbd18c450dc5a33
hantao <hantaoemail@sina.cn>
2020-04-07 19:21:38 +0800

    添加子单拆单数量表

---
+++ src/main/java/com/aiqin/mgs/order/api/dao/OrderSplitsNumDao.java
@@ -0,0 +1,18 @@
+package com.aiqin.mgs.order.api.dao;
+
+import com.aiqin.mgs.order.api.domain.OrderSplitsNum;
+
+public interface OrderSplitsNumDao {
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(OrderSplitsNum record);
+
+    int insertSelective(OrderSplitsNum record);
+
+    OrderSplitsNum selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(OrderSplitsNum record);
+
+    int updateByPrimaryKey(OrderSplitsNum record);
+}
