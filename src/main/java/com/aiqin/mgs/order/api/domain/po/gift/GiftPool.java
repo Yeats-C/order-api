1d970fdf43032ebbb1163f3d5ac9f3daaef43a17
chensifang <762316017@qq.com>
2020-04-09 10:06:25 +0800

    erp使用赠品池列表  筛选参数bean 加上@ApiModel注解

--- src/main/java/com/aiqin/mgs/order/api/domain/po/gift/GiftPool.java
+++ src/main/java/com/aiqin/mgs/order/api/domain/po/gift/GiftPool.java
@@ -4,6 +4,7 @@ import com.aiqin.mgs.order.api.base.PagesRequest;
 import com.aiqin.mgs.order.api.domain.request.product.StockBatchRespVO;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
@@ -19,6 +20,7 @@ import java.util.List;
  * @date 2020/04/04 17:18
  */
 @Data
+@ApiModel("兑换赠品池明细")
 @JsonInclude(JsonInclude.Include.ALWAYS)
 public class GiftPool extends PagesRequest {
 
