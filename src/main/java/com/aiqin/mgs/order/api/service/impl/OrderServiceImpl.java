c40c0a277fff0244f2481e973c97a3a72828d322
jinghaibo <jinghaibo@fotrontimes.com>
2020-04-21 11:42:00 +0800

    是否可退货

--- src/main/java/com/aiqin/mgs/order/api/service/impl/OrderServiceImpl.java
+++ src/main/java/com/aiqin/mgs/order/api/service/impl/OrderServiceImpl.java
@@ -391,13 +391,14 @@ public class OrderServiceImpl implements OrderService {
             //设置不可退货状态
             if (one.getOrderStatus().equals(Global.ORDER_STATUS_0)) {
                 one.setTurnReturnView(1);
-            } else if (one.getOrderStatus().equals(Global.ORDER_STATUS_5) || one.getOrderStatus().equals(Global.ORDER_STATUS_6)) {
+            } else if (one.getOrderStatus().equals(Global.ORDER_STATUS_5) || one.getOrderStatus().equals(Global.ORDER_STATUS_6)|| one.getOrderStatus().equals(Global.ORDER_STATUS_2)) {
                 //设置不可退货状态：
                 //可退货 ：已提货-已提货退货 >0
+                //可退货 ：购买数量-已提货-未提货退货 >0
                 List<PrestorageOrderSupplyDetail> p = prestorageOrderSupplyDetailDao.selectPrestorageOrderDetailByOrderId(one.getOrderId());
                 one.setTurnReturnView(1);
                 for (PrestorageOrderSupplyDetail prestorageOrderSupplyDetail : p) {
-                    if (prestorageOrderSupplyDetail.getSupplyAmount() > prestorageOrderSupplyDetail.getReturnAmount()) {
+                    if (prestorageOrderSupplyDetail.getAmount()-prestorageOrderSupplyDetail.getSupplyAmount() - prestorageOrderSupplyDetail.getReturnPrestorageAmount()>0) {
                         one.setTurnReturnView(0);
                     }
                 }
