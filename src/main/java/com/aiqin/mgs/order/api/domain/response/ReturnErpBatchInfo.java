da975f8cc98e5b804605203c12be58f636f58ea3
zhangjunjie <zhang1234>
2020-08-10 19:46:27 +0800

    修改退货批次数据

--- src/main/java/com/aiqin/mgs/order/api/domain/response/ReturnErpBatchInfo.java
+++ src/main/java/com/aiqin/mgs/order/api/domain/response/ReturnErpBatchInfo.java
@@ -1,4 +1,4 @@
-package com.aiqin.mgs.order.api.domain.po.order;
+package com.aiqin.mgs.order.api.domain.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModelProperty;
@@ -13,7 +13,7 @@ import lombok.Data;
  */
 @Data
 //@JsonInclude(JsonInclude.Include.ALWAYS)
-public class ErpBatchInfo{
+public class ReturnErpBatchInfo {
 	
 	@JsonProperty("batch_info_code")
     @ApiModelProperty("批次编码")
@@ -23,6 +23,10 @@ public class ErpBatchInfo{
     @ApiModelProperty("批次号")
     private String batchNo;
 	
+	@ApiModelProperty(value="商品数量")
+	@JsonProperty("product_count")
+	private Long productCount;
+
 	@ApiModelProperty(value="订货数量")
 	@JsonProperty("total_product_count")
 	private Integer totalProductCount;
