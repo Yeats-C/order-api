cda804829d863761e51688e5d2db96a3c43ac963
chensifang <762316017@qq.com>
2020-08-18 19:04:06 +0800

    订单发货-批次信息更新到本地 订单同步结算-关联供应链返回的批次信息

--- src/main/java/com/aiqin/mgs/order/api/domain/OrderBatchStoreDetail.java
+++ src/main/java/com/aiqin/mgs/order/api/domain/OrderBatchStoreDetail.java
@@ -6,14 +6,16 @@ import lombok.Data;
 
 import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.Date;
 @Data
 public class OrderBatchStoreDetail implements Serializable {
     //订单批次明细（仓卡）
     @ApiModelProperty(value = "行号")
     @JsonProperty("line_code")
-
+    @NotEmpty(message = "行号不能为空")
     private Long lineCode;
+
     @ApiModelProperty(value = "SKU编码")
     @JsonProperty("sku_code")
     @NotEmpty(message = "SKU编码不能为空")
@@ -32,4 +34,24 @@ public class OrderBatchStoreDetail implements Serializable {
     @ApiModelProperty(value = "生产日期",example = "2001-01-01 01:01:01")
     @JsonProperty("product_date")
     private Date productDate;
+
+    @ApiModelProperty(value="批次编码")
+    @JsonProperty("batch_info_code")
+    @NotEmpty(message = "批次编码（唯一标识）不能为空")
+    private String batchInfoCode;
+
+    @ApiModelProperty("销售数量")
+    @JsonProperty("total_count")
+    private Long totalCount;
+
+    @ApiModelProperty("实际销售数量")
+    @JsonProperty("actual_total_count")
+    private Long actualTotalCount;
+
+    @ApiModelProperty("传入库房编码:1:销售库，2:特卖库")
+    private String warehouseTypeCode;
+
+    @ApiModelProperty("批次价格")
+    @JsonProperty("batch_price")
+    private BigDecimal batchPrice=new BigDecimal("0");
 }
